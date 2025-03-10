9824d012d2e46f00b0b0e63400f066736d5d95bf
unknown <quan1997ap@gmail.com>
2025-03-11 02:12:29 +0700

    Update user management

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     appVersion: 'v1',
-    // apiUrl: "https://prod-api-tbhh-5uk2.encr.app"
-    apiUrl: '/proxy'
+    apiUrl: "https://prod-api-tbhh-5uk2.encr.app"
+    // apiUrl: '/proxy'
 }
